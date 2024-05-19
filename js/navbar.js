324ac0aa4f87d20586c7139e89dfd3de7ea8d336
sakshi8994 <sbgaikwad662003@gmail.com>
2024-05-19 14:40:10 +0530

    animation added and color - font

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,11 @@
+// navbar.js
 document.querySelector(".dropdown-btn").addEventListener("click", function() {
-    document.querySelector(".nav-links").classList.toggle("show");
-  });
+  const navLinks = document.querySelector(".nav-links");
+  if (navLinks.classList.contains("expanded")) {
+      navLinks.classList.remove("expanded");
+      navLinks.classList.add("collapsed");
+  } else {
+      navLinks.classList.remove("collapsed");
+      navLinks.classList.add("expanded");
+  }
+});
